d2037007f8bb8381f0c40853550544f3e7a32c3d
AngelMelara <angelmelara44@gmail.com>
2023-09-11 14:42:03 -0600

    archivos creados al compilar

--- public/js/app.js
+++ public/js/app.js
@@ -60667,8 +60667,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\PCHE\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\PCHE\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\CTGXGJ3\Desktop\ProyectoPCHE\PCHE\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\CTGXGJ3\Desktop\ProyectoPCHE\PCHE\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
