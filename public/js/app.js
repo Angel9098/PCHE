0da6dc0d12af6ddd28fbc258863db5cd993f9bee
hector-manny <mejia.martinez2012@hotmail.es>
2023-09-12 11:41:54 -0600

    Maquetación de pantalla de calendario
    se agrego la pantalla de calendario

--- public/js/app.js
+++ public/js/app.js
@@ -59950,6 +59950,9 @@ var routes = [{
 }, {
   path: '/historialhoras',
   component: __webpack_require__(/*! ./components/HistorialHorasComponent.vue */ "./resources/js/components/HistorialHorasComponent.vue")["default"]
+}, {
+  path: '/importacionhoras',
+  component: __webpack_require__(/*! ./components/CargaHorasComponent.vue */ "./resources/js/components/CargaHorasComponent.vue")["default"]
 }];
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_0__["default"]({
   routes: routes,
@@ -60076,6 +60079,38 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_loaders_templateLoader_js_ref_6_node_modules_vue_loader_lib_index_js_vue_loader_options_ActivacionUsuarioComponent_vue_vue_type_template_id_8b92b3fe___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
+
+/***/ }),
+
+/***/ "./resources/js/components/CargaHorasComponent.vue":
+/*!*********************************************************!*\
+  !*** ./resources/js/components/CargaHorasComponent.vue ***!
+  \*********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+var render, staticRenderFns
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__["default"])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+component.options.__file = "resources/js/components/CargaHorasComponent.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
 
 /***/ }),
 
@@ -60656,8 +60691,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\PCHE-II\PCHE\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\PCHE-II\PCHE\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\PCHE\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\PCHE\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
