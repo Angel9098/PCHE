4c731509526a2fc5057d5a6498d1bad6f53fcc6e
hector-manny <mejia.martinez2012@hotmail.es>
2023-09-13 10:03:34 -0600

    fix de pantalla de calendario

--- resources/js/components/CalendarioComponent.vue
+++ resources/js/components/CalendarioComponent.vue
@@ -2,57 +2,31 @@
     <div class="container">
         <br>
         <h1 class="h1 text-center">Selección de fecha de corte</h1>
+        <br>
         <div class="content-container">
             <v-date-picker is-expanded v-model="selectedDate" class="vdp-datepicker"></v-date-picker>
-            <textarea placeholder="Escribe algo aquí..." class="textbox" rows="4" disabled></textarea>
+            <textarea placeholder=" " class="textbox" rows="10" disabled></textarea>
         </div>
     
-        <h1 class="h1 text-center mt-5">Historial de fechas de corte</h1>
-        <div class="card bg-white borderCircle">
-            <div class="card-body">
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                    <label class="form-check-label" for="flexSwitchCheckDefault">Mostrar Filtros</label>
-                </div>
-                <div class="d-flex flex-row gap-3 justify-content-between align-items-center mt-3">
-                    <div class="col-6 form-group">
-                        <input type="text" class="form-control" id="search" :placeholder="'Buscar...'">
-                    </div>
-                    <div class="col-4">
-                        <select class="form-select">
-                            <option value="activo">Activo</option>
-                            <option value="inactivo">Inactivo</option>
-                        </select>
-                    </div>
-                </div>
-                
+        <h1 class="h1 text-center mt-5">Historial de fechas de corte</h1>               
                 <table class="table table-hover table-bordered mt-4">
                     <thead class="table-dark">
                         <tr class="text-center">
-                            <th scope="col">Nombre</th>
-                            <th scope="col">DUI</th>
-                            <th scope="col">Cargo</th>
-                            <th scope="col">Horas</th>
-                            <th scope="col">Fecha</th>
-                            <th scope="col">Estado</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Fecha de corte</th>
+                            <th scope="col">Fecha de creación</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Juan Pérez</td>
+                        <tr class="text-center">
+                            <td>Corte del 14 de septiembre</td>
                             <td>01234569-7</td>
                             <td>Desarrollador</td>
-                            <td>12</td>
-                            <td>31/07/2023</td>
-                            <td>Activo</td>
                         </tr>
-                        <tr>
+                        <tr class="text-center">
                             <td>Juan Pérez</td>
                             <td>01234569-7</td>
                             <td>Desarrollador</td>
-                            <td>12</td>
-                            <td>31/07/2023</td>
-                            <td>Activo</td>
                         </tr>
                     </tbody>
                 </table>
@@ -75,8 +49,6 @@
                     </ul>
                 </nav>
             </div>
-        </div>
-    </div>
 </template>
 
 <script>
