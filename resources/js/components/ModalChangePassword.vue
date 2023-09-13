c1caf7f8dafb4754d512f9dba9b065a2afe45322
Cristian Zayas <josecristianzayas@gmail.com>
2023-09-13 18:10:30 +0200

    Creación del menu y de actualización en la pantalla de perfil

--- resources/js/components/ModalChangePassword.vue
+++ resources/js/components/ModalChangePassword.vue
@@ -1,3 +1,55 @@
+
 <template>
-    
+  <form>
+    <div class="form-group">
+      <label for="InputActual">Contraseña actual</label>
+      <input
+        type="password"
+        class="form-control"
+        id="InputActual"
+        aria-describedby="emailHelp"
+        required
+        v-model="changesPassowrd.oldPassword"
+      />
+      <small id="emailHelp" class="form-text text-muted"
+        >We'll never share your email with anyone else.</small
+      >
+    </div>
+    <div class="form-group">
+      <label for="InputPasswordNueva">Nueva contraseña</label>
+      <input v-model="changesPassowrd.newPassword" type="password" class="form-control" id="InputPasswordNueva" required />
+    </div>
+    <div class="form-group">
+      <label for="InputPasswordRepita">Repita la contraseña</label>
+      <input v-model="changesPassowrd.confirmPassword" type="password" class="form-control" id="InputPasswordRepita" required />
+    </div>
+    <button @click="sendPassword()" type="submit" class="btn btn-dark my-4">
+      Actualizar credenciales
+    </button>
+  </form>
 </template>
+  
+  <script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      changesPassowrd: {
+        oldPassword: "",
+        newPassword: "",
+        confirmPassword: "",
+      },
+    };
+  },
+  methods: {
+    sendPassword() {
+      console.log(this.changesPassowrd.oldPassword);
+       axios.post('/actualizarcontra',this.changesPassowrd).then((response)=>{
+        console.log(response)
+       }).catch((error) => {
+        console.log(error)
+       })
+    },
+  },
+};
+</script>
